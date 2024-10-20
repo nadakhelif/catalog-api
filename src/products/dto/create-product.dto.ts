68b8688652a7fb47acac0ff2565277de5832de22
nada <nadakhelif61@gmail.com>
2024-10-20 17:00:52 +0100

    added product dto

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,16 @@
-export class CreateProductDto {}
+import { IsString, IsNumber, IsBoolean, Min } from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  description: string;
+
+  @IsNumber()
+  @Min(0)
+  price: number;
+
+  @IsBoolean()
+  isConnectedOnly: boolean;
+}
