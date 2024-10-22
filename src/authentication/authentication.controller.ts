c1d7dd4e8739793bdb32f5499cab8132c8ad28b2
nada <nadakhelif61@gmail.com>
2024-10-22 01:08:18 +0100

    add description to swagger in auth controller

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -1,12 +1,20 @@
 import { Body, Controller, Post, UnauthorizedException } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import { SignUpDto } from './dto/sign-up.dto';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('authentication')
 @Controller('authentication')
 export class AuthenticationController {
   constructor(private authService: AuthenticationService) {}
 
   @Post('login')
+  @ApiOperation({ summary: 'Login a user' })
+  @ApiBody({
+    description: 'User credentials for login',
+  })
+  @ApiResponse({ status: 200, description: 'Successful login' })
+  @ApiResponse({ status: 401, description: 'Invalid credentials' })
   async login(@Body() loginDto: { email: string; password: string }) {
     const { email, password } = loginDto;
     const user = await this.authService.validateUser(email, password);
@@ -17,6 +25,10 @@ export class AuthenticationController {
     return this.authService.login(user);
   }
   @Post('signup')
+  @ApiOperation({ summary: 'Register a new user' })
+  @ApiBody({ type: SignUpDto })
+  @ApiResponse({ status: 201, description: 'User successfully registered' })
+  @ApiResponse({ status: 400, description: 'Bad request' })
   async signup(@Body() signUpDto: SignUpDto) {
     await this.authService.signup(signUpDto);
   }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const catalogApiConfig = new DocumentBuilder()
+    .setTitle('Catalog API')
+    .setDescription('API for managing product catalogs')
+    .setVersion('1.0')
+    .addTag('catalog')
+    .build();
+  const document = SwaggerModule.createDocument(app, catalogApiConfig);
+  SwaggerModule.setup('api/docs', app, document);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
